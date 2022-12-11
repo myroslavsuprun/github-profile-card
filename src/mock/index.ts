ab324c9f1af6df7167ac670f17a2179639596074
myroslavsuprun <sirmiroslavsuprun@gmail.com>
2022-12-11 15:59:51 +0200

    Github user profile card completed

---
+++ src/mock/index.ts
@@ -0,0 +1,16 @@
+import { LocalGithubUser } from 'types';
+
+export const defaultUser: LocalGithubUser = {
+  login: 'myroslavsuprun',
+  avatar: 'https://avatars.githubusercontent.com/u/88524154?v=4',
+  name: 'Myroslav Suprun',
+  company: '',
+  blog: '',
+  location: 'Riga, Latvia',
+  bio: 'React developer',
+  twitter: '',
+  repos: 31,
+  followers: 5,
+  following: 20,
+  created: '2021-08-06T03:57:35Z',
+};
